200740cb68507a87a8e6003fa718c74a6ac6a782
Parity Account <adaq1.jlab.org>
2019-07-27 17:57:01 -0400

    Cameron: Fixing a typo in one of the agg_list scripts that crept in

--- agg-scripts/aggregator_list_slopes.sh
+++ agg-scripts/aggregator_list_slopes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/PREX/prompt/Aggregator/wrapper/
 ../setup_camguin.sh
-python loop.py -r ${1} -f slope_input.txt -F 1 -s ${2}# Do miniruns
-python loop.py -r ${1} -f slope_input.txt -F 0 -s ${2}# Do full runs
+python loop.py -r ${1} -f slope_input.txt -F 1 -s ${2} # Do miniruns
+python loop.py -r ${1} -f slope_input.txt -F 0 -s ${2} # Do full runs
