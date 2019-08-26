50f18a729319a4992e28d3a4933ddee8db6614ae
Robert Radloff <rradloff@jlab.org>
2019-08-26 12:15:47 -0400

    First set of changes trying to get the prompt to work on the farm.
    Most of the changes are just file permissiosn, which was done to allow more than one person to make changes in the directory structure.
    
    Substantive changes are in the summary.sh script where we've added diagnostic output to tell where the files are getting created (or not, as the case may be).

--- summary.sh
+++ summary.sh
@@ -1,58 +1,75 @@
 #!/bin/sh
+scriptDir=`pwd`
+cd $WORK_DIR
 
 runnum=$1
 level="Prompt"
 shopt -s extglob
 # find split file
-rootfile_list=$(ls -1 ./japanOutput/prex$level\_pass1_$runnum.!(*jlab.org*).root);
+rootfile_list=$(ls -1 ${scriptDir}/japanOutput/prex$level\_pass1_$runnum.!(*jlab.org*).root);
 shopt -u extglob
+
+alias convert /u/group/halla/parity/software/japan_offline/prompt/prex-prompt/convert
+which convert
 
 for rootfile  in $rootfile_list
 do
     # strip out [run.seg]
+    echo "Starting to run summary.sh for $rootfile from "`pwd`
 
     trim=${rootfile%.root}
     run_dot_seg=${trim#*pass1_}
     run_num=${run_dot_seg%.*}
     run_seg=${run_dot_seg/./_}
 
-    redfile='./results/prexPrompt_'${run_seg}'_regress_postpan.root';
+    redfile="${scriptDir}/results/prexPrompt_${run_seg}_regress_postpan.root";
     
     if [ ! -d ./tmp/run$run_seg ]; then
 	mkdir ./tmp/run$run_seg;
     fi
+    chmod 777 ./tmp/run$run_seg;
+    echo "Plots should be going to ./tmp/run$run_seg"
     
-    root -b -q -l './rootMacros/PlotSummary.C("'$rootfile'")';
-    root -b -q -l './postpan/rootmacros/PlotSummary_postpan.C("'$redfile'")';
+    root -b -q -l "${scriptDir}/rootMacros/PlotSummary.C(\"${rootfile}\")";
+    root -b -q -l "${scriptDir}/postpan/rootmacros/PlotSummary_postpan.C(\"${redfile}\")";
+
+    echo "****  CHECKING THE DIRECTORY STUCTURE"
+    pwd
+    echo "ls -lrtd `pwd`/tmp/run$run_seg"
+    ls -lrtd `pwd`/tmp/run$run_seg
+    echo "ls -lrt `pwd`/tmp/run$run_seg"
+    ls -lrt `pwd`/tmp/run$run_seg
+    echo "****  DONE CHECKING THE DIRECTORY STUCTURE"
 
     # pdfunite $(ls -rt ./tmp/run$run_seg/*_summary_*.pdf) \
     # 	./tmp/run$run_seg/run${run_seg}_all.pdf
 
-    if [ ! -d ./hallaweb_online/summary/run$run_seg ]; then
-	mkdir ./hallaweb_online/summary/run$run_seg;
+    if [ ! -d ${scriptDir}/hallaweb_online/summary/run$run_seg ]; then
+	mkdir ${scriptDir}/hallaweb_online/summary/run$run_seg;
     fi
     
     mv  ./tmp/run$run_seg/* \
 	./hallaweb_online/summary/run$run_seg/;
     rm  -rf ./tmp/run$run_seg;
+
     # copying prompt summary
     cp ./japanOutput/summary_*$runnum*.txt \
-	./SummaryText/
+	${scriptDir}/SummaryText/
 
-    mv  ./SummaryText/summary_$run_num.txt \
-	./SummaryText/summary_$level_$run_seg.txt \
+    mv  ${scriptDir}/SummaryText/summary_$run_num.txt \
+	${scriptDir}/SummaryText/summary_$level_$run_seg.txt \
 
-    cp  ./SummaryText/summary_$level_$run_seg.txt \
-	./hallaweb_online/summary/run$run_seg/ ;
+    cp  ${scriptDir}/SummaryText/summary_$level_$run_seg.txt \
+	${scriptDir}/hallaweb_online/summary/run$run_seg/ ;
     # copying postpan summary
-    cp  ./results/prexPrompt_$run_seg\_postpan_summary.txt \
-    	./hallaweb_online/summary/run$run_seg/;
+    cp  ${scriptDir}/results/prexPrompt_$run_seg\_postpan_summary.txt \
+    	${scriptDir}/hallaweb_online/summary/run$run_seg/;
 
     #Change user group and permission
-    chgrp -R a-parity ./hallaweb_online/summary/run$run_seg;
-    chmod -R 755 ./hallaweb_online/summary/run$run_seg;    
+    chgrp -R a-parity ${scriptDir}/hallaweb_online/summary/run$run_seg;
+    chmod -R 777 ${scriptDir}/hallaweb_online/summary/run$run_seg;    
     
-    bash 	./hallaweb_online/summary/sort.sh ;
+    bash 	${scriptDir}/hallaweb_online/summary/sort_farm.sh ;
 done
 
 
