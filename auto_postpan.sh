819329357f82ced5a39c00f8b04478cb99be739c
Tao Ye <tao.ye@stonybrook.edu>
2019-10-31 13:08:11 -0400

    re-arrange postpan configs

--- auto_postpan.sh
+++ auto_postpan.sh
@@ -23,7 +23,11 @@ do
     postpanConf="combo_reg.conf"
     if [ $(($run_num)) -ge 3390 ]
     then
-      postpanConf="combo_reg.3390-3582.conf"
+      postpanConf="combo_reg.3390-3402.conf"
+    fi
+    if [ $(($run_num)) -ge 3403 ]
+    then
+      postpanConf="combo_reg.3403-3582.conf"
     fi
     if [ $(($run_num)) -ge 3583 ]
     then
@@ -31,11 +35,11 @@ do
     fi
     if [ $(($run_num)) -ge 3803 ]
     then
-      postpanConf="combo_reg.3803-4294.conf"
+      postpanConf="combo_reg.3803-4304.conf"
     fi
-    if [ $(($run_num)) -ge 4295 ]
+    if [ $(($run_num)) -ge 4305 ]
     then
-      postpanConf="combo_reg.4295-.conf"
+      postpanConf="combo_reg.4305-.conf"
     fi
 
     ./postpan/redana \
