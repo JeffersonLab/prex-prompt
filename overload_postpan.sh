6b4e80db2516b23f1ad043d2e8d6601c6690ce73
Tao Ye <tao.ye@stonybrook.edu>
2019-11-13 09:00:56 -0500

    Config files for regression and lagrange analyses

---
+++ overload_postpan.sh
@@ -0,0 +1,53 @@
+#! /bin/sh
+runnum=$1;
+
+if [ -z "$runnum" ] 
+then
+    echo "Run Number is empty";
+    exit 1;
+fi    
+
+level="Prompt"
+shopt -s extglob
+# find split file
+rootfile_list=$(ls -1 ./japanOutput/prex$level\_pass1_$runnum.!(*farm*).root);
+shopt -u extglob
+
+for rootfile  in $rootfile_list
+do
+    trim=${rootfile%.root}
+    run_dot_seg=${trim#*pass1_}
+    run_num=${run_dot_seg%.*}
+    run_seg=${run_dot_seg/./_}
+
+    overloadConf="overload_reg.conf"
+    comboBPMConf="comboBPM_reg.conf" 
+
+    if [ $(($run_num)) -ge 3390 ]
+    then
+      overloadConf="overload_reg.3390-3802.conf"
+      comboBPMConf="comboBPM_reg.3390-3802.conf"
+    fi
+    if [ $(($run_num)) -ge 3803 ]
+    then
+      overloadConf="overload_reg.3803-4294.conf"
+      comboBPMConf="comboBPM_reg.3803-4294.conf"
+    fi
+    if [ $(($run_num)) -ge 4295 ]
+    then
+      overloadConf="overload_reg.4295-.conf"
+      comboBPMConf="comboBPM_reg.4295-.conf"
+    fi
+
+    ./postpan/redana \
+    	-f $rootfile \
+    	-c ./postpan/conf/$overloadConf ; 
+
+    # if[ $(($run_num)) -ge 3390 ] then
+    # 	./postpan/redana \
+    # 	    -f $rootfile \
+    # 	    -c ./postpan/conf/$comboBPMConf;
+    # fi
+
+done
+
