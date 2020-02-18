2b0d3031df6dd28bd913b8ae438fd86c17e8bfa2
Parity Account <adaq1.jlab.org>
2020-02-18 17:52:42 -0500

    Cameron: updating aggregator script to have a run ranged input file

--- aggregator.sh
+++ aggregator.sh
@@ -7,5 +7,14 @@ ls -lsatrH ~/PREX/prompt/aggRootfiles/run_aggregator_${1}.root
 rm -f ~/PREX/prompt/aggRootfiles/minirun_aggregator_${1}_*.root
 rm -f ~/PREX/prompt/aggRootfiles/run_aggregator_${1}.root
 echo "Creating aggregator files for run $1"
-python loop-oneRun.py -r ${1} -f input.txt -b prexPrompt_pass1 -F 1 # Do full runs
-python loop-oneRun.py -r ${1} -f input.txt -b prexPrompt_pass1 -F 0 # Do mini runs
+conf="input.txt"
+if [ $(($run_num)) -ge 5337 ]
+then
+  conf="input.5337-6490.conf"
+fi
+if [ $(($run_num)) -ge 6491 ]
+then
+  conf="input.txt"
+fi
+python loop-oneRun.py -r ${1} -f $conf -b prexPrompt_pass1 -F 1 # Do full runs
+python loop-oneRun.py -r ${1} -f $conf -b prexPrompt_pass1 -F 0 # Do mini runs
--- dither_summary.sh
+++ dither_summary.sh
@@ -4,6 +4,7 @@ cd ~/PREX/prompt/bmodAna
 set slug=`rcnd ${1} slug`
 root -l -b -q bmodAna.C\($1,\"inputs/input_CREX_ye.txt\"\);
 root -l -b -q runBmodAna.C\($1,\"inputs/input_CREX_run_avg.txt\"\);
+root -l -b -q runBmodAna.C\($1,\"inputs/input_CREX_run_avg_1X.txt\"\);
 root -l -b -q plotAD_13746.C\(\"slopes/dithering_slopes_13746_slug${slug}.root\",\"$1\"\);
 #root -l -b -q bmodAna.C\($1,\"inputs/max_input.txt\"\);
 #root -l -b -q plotAD.C\(\"slopes/dithering_slopes_1133557664422_slug${slug}_run${1}.root\"\);
