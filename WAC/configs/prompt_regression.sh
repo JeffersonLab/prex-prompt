73fd31cb4d92c29b2683faba612733cd83254da4
Parity Account <adaq1.jlab.org>
2020-06-16 18:05:19 -0400

    Cameron: Adding a new streamlined aggregator submission system This one uses a more easily visible and editted global variable setting script and bypasses the wrapper.sh script.

--- WAC/configs/prompt_regression.sh
+++ WAC/configs/prompt_regression.sh
@@ -1,4 +1,20 @@
+# Making grand plots: 
+
+## adaq computer settings
 source ~/PREX/setup_japan.sh
 export JAPAN_DIR=~/PREX/japan
 export CAM_TYPE=""
+export CAM_CONFIG="input.txt"
 export CAM_OUTPUTDIR=/chafs2/work1/apar/aggRootfiles/${CAM_TYPE}
+
+
+# Making agg-outputs:
+
+## adaq computer settings 
+export RCDB_CONNECTION=mysql://rcdb@hallcdb.jlab.org:3306/a-rcdb
+export ROOTSYS=/adaqfs/apps/ROOT/6.16-00
+export PATH="${ROOTSYS}/bin:${PATH}"
+export LD_LIBRARY_PATH="${ROOTSYS}/lib:${LD_LIBRARY_PATH}"
+export CAM_OUTPUTDIR=/chafs2/work1/apar/aggRootfiles/
+export POSTPAN_ROOTFILES=/chafs2/work1/apar/postpan-outputs/
+export LRB_ROOTFILES=/chafs2/work1/apar/LRBoutput/
