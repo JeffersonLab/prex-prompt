95962e04eb652b1cc6043230ccf015800a35c20c
Parity Account <adaq1.jlab.org>
2020-06-18 17:36:35 -0400

    Cameron: Debugging new aggregator framework  - Adding a line to make sure the destination directory exists  - Adding a test config - does "default" device list + friends all possible files (I noticed a bug in the process ;) )

--- WAC/any_aggregator.sh
+++ WAC/any_aggregator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ${2}
-if [ "$#" -eq 1 ]; then
+if [[ $# -eq 1 && "$CAM_CONFIG" == "" ]]; then
   source ~/PREX/prompt/WAC/configs/prompt_regression.sh
 fi
 # Backup default case
@@ -10,12 +10,15 @@ fi
 if [[ "$JAPAN_DIR" == "" ]] ; then 
   export JAPAN_DIR="~/PREX/japan"
 fi
-cd ${JAPAN_DIR}/rootscripts/aggregator/wrapper/
+cd ${JAPAN_DIR}/rootScripts/aggregator/wrapper/
 echo "Deleting any old aggregator files for run $1:"
 ls -lsatrH $CAM_OUTPUTDIR/minirun_aggregator_${1}_*.root
 ls -lsatrH $CAM_OUTPUTDIR/run_aggregator_${1}.root
 rm -f $CAM_OUTPUTDIR/minirun_aggregator_${1}_*.root
 rm -f $CAM_OUTPUTDIR/run_aggregator_${1}.root
+if [ ! -d $CAM_OUTPUTDIR ]; then 
+  mkdir $CAM_OUTPUTDIR
+fi
 echo "Creating aggregator files for run $1:"
 python any-loop-oneRun.py -r ${1} -f ${CAM_CONFIG} -F 1 # Do full runs # -b prexPrompt_pass2
 python any-loop-oneRun.py -r ${1} -f ${CAM_CONFIG} -F 0 # Do mini runs # -b prexPrompt_pass2
